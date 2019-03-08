5e6e7e5abbed13505ccf2994a16965ebd938e6fd
kaburayalab <sns@kabura-ya.net>
2019-03-08 19:14:35 +0900

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -5,4 +5,9 @@ apt install mosquitto -y
 apt install mysql-server mysql-client -y
 apt install php -y
 sudo apt install phpmyadmin -y
+cp phpmyadmin.conf /etc/httpd/conf.d/phpmyadmin.conf
+chown -R www-data /usr/share
+sudo mkdir /usr/share/phpmyadmin/config
+chmod 777 /usr/share/phpmyadmin/config
+chown -R www-data /usr/share
 mysql_secure_installation
