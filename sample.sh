51c9df6c0017d311d7012ed72a1cdcf7fe3aa89d
kaburayalab <sns@kabura-ya.net>
2019-03-08 17:43:13 +0900

    Install httpd, mysql, PHP, phpMyAdmin, Mosquitto

---
+++ sample.sh
@@ -0,0 +1,8 @@
+apt upgrade
+apt update
+apt install apache2 -y
+apt install mosquitto -y
+apt install mysql-server mysql-client -y
+apt install php -y
+sudo apt install phpmyadmin -y
+mysql_secure_installation
